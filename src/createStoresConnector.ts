e1be0902b63cb0ff0c2af1c6960b4ba3105350ba
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-12-02 18:43:59 +0700

    release: v.0.7.0-alpha.2

--- src/createStoresConnector.ts
+++ src/createStoresConnector.ts
@@ -7,7 +7,13 @@ interface IStoresConnectorOptions {
 
 export const createStoresConnector = (options: IStoresConnectorOptions = {}): IEventStoresConnector => ((Options): IEventStoresConnector => {
   const { uri = "mongodb://localhost:27017" } = Options;
-  const Client = new MongoClient(uri, { useNewUrlParser: true });
+  const Client = new MongoClient(
+    uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
   return {
     connect: async (scope, type) => {
